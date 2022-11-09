1978fe16017a801d21ead85b28daf13fb7011b66
ZAMANXD <znsnlab@gmail.com>
2022-11-09 23:36:18 +0600

    hero section added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../Layout/Main';
 import About from '../../pages/About/About';
-import Home from '../../pages/Home/Home';
+import Home from '../../pages/Home/Home/Home';
 import Join from '../../components/Join/Join';
 import Reviews from '../../pages/Reviews/Reviews';
 import Blog from '../../pages/Blog/Blog';
--- src/pages/Home/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Home.css';
-
-const Home = () => {
-  return <div className="befr-min-height">This is home</div>;
-};
-
-export default Home;
